ad29ec09187e6be23635bd5b0eb538800b00d892
Bartosz Gajda <bg@bartoszgajda.com>
2017-04-18 18:07:09 +0100

    Moved navbar to different component.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { MainComponent } from './components/main/main.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   imports: [
@@ -10,7 +11,8 @@ import { MainComponent } from './components/main/main.component';
     DashboardRoutingModule
   ],
   declarations: [
-    MainComponent
+    MainComponent,
+    NavbarComponent
   ]
 })
 export class DashboardModule { }
