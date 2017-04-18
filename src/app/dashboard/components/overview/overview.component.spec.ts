5b2853846c83430a3ece0ecf596eaf272c52b0a0
Bartosz Gajda <bg@bartoszgajda.com>
2017-04-18 19:33:02 +0100

    Refactored component: default -> overview. Deleted dummy text from dashboard.

--- src/app/dashboard/components/overview/overview.component.spec.ts
+++ src/app/dashboard/components/overview/overview.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DefaultComponent } from './default.component';
+import { OverviewComponent } from './overview.component';
 
 describe('DefaultComponent', () => {
-  let component: DefaultComponent;
-  let fixture: ComponentFixture<DefaultComponent>;
+  let component: OverviewComponent;
+  let fixture: ComponentFixture<OverviewComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DefaultComponent ]
+      declarations: [ OverviewComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DefaultComponent);
+    fixture = TestBed.createComponent(OverviewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
