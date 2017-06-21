ed248a7d9b73655d08e382c917278691d2d888dc
Bartosz Gajda <bg@bartoszgajda.com>
2017-06-21 22:06:03 +0100

    Moved auth logic into separate service.

--- src/app/home/components/login/login.component.ts
+++ src/app/home/components/login/login.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AngularFireAuth} from "angularfire2/auth";
-import {Router} from "@angular/router";
-import {Observable} from "rxjs/Observable";
-import * as firebase from 'firebase/app';
+import {AuthService} from "../../../shared/services/auth/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -13,26 +10,15 @@ export class LoginComponent implements OnInit {
   email: string;
   password: string;
 
-  user: Observable<firebase.User>;
-
   constructor(
-    public afAuth: AngularFireAuth,
-    private router: Router
-  ) {
-    this.user = afAuth.authState;
-  }
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
   }
 
   onLogin() {
-    this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password).catch(e => {
-      if(e) {
-        console.log(e);
-      }
-    }).then(a => {
-      this.router.navigate(['/dashboard']);
-    });
+    this.authService.loginUserWithEmail(this.email, this.password);
   }
 
 }
--- src/app/home/components/register/register.component.ts
+++ src/app/home/components/register/register.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from "rxjs/Observable";
-import * as firebase from 'firebase/app';
-import {AngularFireAuth} from "angularfire2/auth";
-import {Router} from "@angular/router";
+import {AuthService} from "../../../shared/services/auth/auth.service";
 
 @Component({
   selector: 'app-register',
@@ -13,25 +10,15 @@ export class RegisterComponent implements OnInit {
   email: string;
   password: string;
 
-  user: Observable<firebase.User>;
-
   constructor(
-    public afAuth: AngularFireAuth,
-    private router: Router
-  ) {
-    this.user = afAuth.authState;
-  }
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
   }
 
   onRegister() {
-    this.afAuth.auth.createUserWithEmailAndPassword(this.email, this.password).catch(e => {
-        if(e)
-          console.log(e);
-        else
-          this.router.navigate(['/dashboard']);
-    });
+    this.authService.registerUserWithEmail(this.email, this.password);
   }
 
 }
