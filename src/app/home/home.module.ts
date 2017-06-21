8dd791d4ed67dcb7050eac173199bb450f587a81
Bartosz Gajda <bg@bartoszgajda.com>
2017-06-21 22:42:48 +0100

    Fixed templates, routes and router outlets.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomeRoutingModule } from './home-routing.module';
 import {LandingPageComponent} from "./components/landing-page/landing-page.component";
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import {FormsModule} from "@angular/forms";
 import {AngularFireAuthModule} from "angularfire2/auth";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutingModule,
     FormsModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    RouterModule
   ],
   declarations: [
     LandingPageComponent,
