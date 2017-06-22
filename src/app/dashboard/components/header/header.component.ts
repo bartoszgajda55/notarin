d55e528987fdbe1608bcf2112960c7b60b24ea06
Bartosz Gajda <bg@bartoszgajda.com>
2017-06-22 21:27:41 +0100

    Observables testing.

--- src/app/dashboard/components/header/header.component.ts
+++ src/app/dashboard/components/header/header.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthService} from "../../../shared/services/auth/auth.service";
 import {Router} from "@angular/router";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  user: {};
+export class HeaderComponent implements OnInit, OnDestroy {
+  user: Subscription;
+  userName: string;
 
   constructor(
     private authService: AuthService,
@@ -16,13 +18,25 @@ export class HeaderComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.user = this.authService.getLoggedUserData();
-    console.log(this.user);
+    this.user = this.authService.getLoggedUser().subscribe((data) => {
+      this.userName = data.toJSON().displayName;
+      console.log(data);
+    });
   }
 
   onLogout() {
     this.authService.logOutUser();
     this.router.navigate(['/login']);
+  }
+
+  onLogData() {
+    this.authService.getLoggedUser().subscribe((data) => {
+      console.log(data);
+    });
+  }
+
+  ngOnDestroy() {
+    this.user.unsubscribe();
   }
 
 }
--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -4,13 +4,10 @@ import {Router} from "@angular/router";
 import {Observable} from "rxjs/Observable";
 import * as firebase from 'firebase/app';
 import 'rxjs';
-import {Subscription} from "rxjs/Subscription";
 
 @Injectable()
 export class AuthService {
   user: Observable<firebase.User>;
-  userSubscription: Subscription;
-  userData: {};
 
   constructor(
     public afAuth: AngularFireAuth,
@@ -21,28 +18,21 @@ export class AuthService {
 
   loginUserWithEmail(email: string, password: string): void {
     this.afAuth.auth.signInWithEmailAndPassword(email, password).then(() => {
-      this.userSubscription = this.user.subscribe((data) => {
-        this.userData = data.toJSON();
-      });
       this.router.navigate(['/dashboard']);
     }).catch(e => { console.log(e) });
   }
 
   registerUserWithEmail(email: string, password: string): void {
     this.afAuth.auth.createUserWithEmailAndPassword(email, password).then(() => {
-      this.userSubscription = this.user.subscribe((data) => {
-        this.userData = data.toJSON();
-      });
       this.router.navigate(['/dashboard']);
     }).catch(e => { console.log(e) });
   }
 
   logOutUser(): void {
     this.afAuth.auth.signOut();
-    this.userSubscription.unsubscribe();
   }
 
-  getLoggedUserData(): object {
-    return this.userData;
+  getLoggedUser(): Observable<any> {
+    return this.user;
   }
 }
