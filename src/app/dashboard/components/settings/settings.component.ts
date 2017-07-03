0d82ec3bfd7754f6542778d88e4d0f3fb1d043c9
Bartosz Gajda <bg@bartoszgajda.com>
2017-07-03 19:37:36 +0100

    Added some setting component logic and simple template text.

--- src/app/dashboard/components/settings/settings.component.ts
+++ src/app/dashboard/components/settings/settings.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AuthService} from "../../../shared/services/auth/auth.service";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent implements OnInit, OnDestroy {
+  private sub: Subscription;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.sub = this.authService.getLoggedUser().subscribe((value) => {
+
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
